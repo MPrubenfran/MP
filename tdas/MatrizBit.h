b4f8784f6b4e540f3fc777adbec3a3a1c6013599
Rubén Morales Pérez <srmorales@correo.ugr.es>
2015-03-25 18:02:00 +0100

    primer commit

---
+++ tdas/MatrizBit.h
@@ -0,0 +1,29 @@
+#ifndef matrizbit_h
+#define matrizbit_h
+
+#include <iostream>
+#include "MatrizBit.h"
+using namespace std;
+
+struct MatrizBit1{
+	int filas, columnas;
+	bool matriz[10][10];
+};
+
+bool Inicializar (MatrizBit& m, int fils, int fils, int cols);
+int Filas (const MatrizBit& m);
+int Columnas (const MatrizBit& m);
+bool Get(const MatrizBit& m,int f,int c);
+void Set(MatrizBit& m,int f,int c,bool v);
+bool Leer(std::istream& is,MatrizBit& m);
+bool Escribir(std::ostream& os,const MatrizBit& m);
+bool Leer(const char nombre[],MatrizBit& m);
+bool Escribir(const char nombre[], const MatrizBit& m);
+void Traspuesta(MatrizBit& res,const MatrizBit& m);
+void And(MatrizBit& res,const MatrizBit& m1,const MatrizBit& m2);
+void Or(MatrizBit& res,const MatrizBit& m1,const MatrizBit& m2);
+void Not(MatrizBit& res,const MatrizBit& m);
+
+
+
+#endif
