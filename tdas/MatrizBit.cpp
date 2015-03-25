10c741bd64fc786dfdccade516a3f6df8c01efee
Rubén Morales Pérez <srmorales@correo.ugr.es>
2015-03-25 18:57:29 +0100

    Update MatrizBit.cpp

--- tdas/MatrizBit.cpp
+++ tdas/MatrizBit.cpp
@@ -3,11 +3,23 @@
 using namespace std;
 
 bool Inicializar (MatrizBit& m, int fils, int fils, int cols);
-int Filas (const MatrizBit& m);
-int Columnas (const MatrizBit& m);
-bool Get(const MatrizBit& m,int f,int c);
-void Set(MatrizBit& m,int f,int c,bool v);
-bool Leer(std::istream& is,MatrizBit& m);
+int Filas (const MatrizBit& m){
+  return m.filas;
+}
+int Columnas (const MatrizBit& m){
+  return m.columnas;
+}
+bool Get(const MatrizBit& m,int f,int c){
+  return m.matriz[f][c];
+}
+void Set(MatrizBit& m,int f,int c,bool v){
+  m.matriz[f][c] = v;
+}
+
+bool Leer(std::istream& is,MatrizBit& m){
+  bool 
+  
+}
 bool Escribir(std::ostream& os,const MatrizBit& m);
 bool Leer(const char nombre[],MatrizBit& m);
 bool Escribir(const char nombre[], const MatrizBit& m);
