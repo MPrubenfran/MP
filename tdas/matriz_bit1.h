8d1a13032d0b98379735b152769829005de1cce3
Rubén Morales Pérez <srmorales@correo.ugr.es>
2015-03-26 15:42:52 +0100

    Update matriz_bit1.h

--- tdas/matriz_bit1.h
+++ tdas/matriz_bit1.h
@@ -1,5 +1,5 @@
-#ifndef matrizbit_h
-#define matrizbit_h
+#ifndef matrizbit1_h
+#define matrizbit1_h
 
 #include <iostream>
 #include "MatrizBit.h"
@@ -11,13 +11,11 @@ struct MatrizBit1{
 };
 
 bool Inicializar (MatrizBit& m, int fils, int fils, int cols);
-int Filas (const MatrizBit& m);
-int Columnas (const MatrizBit& m);
 bool Get(const MatrizBit& m,int f,int c);
 void Set(MatrizBit& m,int f,int c,bool v);
-bool Leer(std::istream& is,MatrizBit& m);
+
 bool Escribir(std::ostream& os,const MatrizBit& m);
-bool Leer(const char nombre[],MatrizBit& m);
+
 bool Escribir(const char nombre[], const MatrizBit& m);
 void Traspuesta(MatrizBit& res,const MatrizBit& m);
 void And(MatrizBit& res,const MatrizBit& m1,const MatrizBit& m2);
