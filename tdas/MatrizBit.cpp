ff8f62d6e7852a4b86dab28763ad32bc5253987b
Rubén Morales Pérez <srmorales@correo.ugr.es>
2015-03-26 15:31:58 +0100

    Update MatrizBit.cpp

--- tdas/MatrizBit.cpp
+++ tdas/MatrizBit.cpp
@@ -1,6 +1,27 @@
+/**
+  * @file matriz_bit.cpp
+  * @brief Fichero de definiciones para insertar la implementación deseada
+  *
+  * Incluye la definición correspondiente dependiendo de la selección en matriz_bit.h
+  * 
+  * @warning Este fichero no se debe modificar
+  *
+  */
 
+#include "matriz_bit.h" // Aquí se define el valor de CUAL_COMPILO
 #include <iostream>
 using namespace std;
+
+#if CUAL_COMPILO==1
+  #include "matriz_bit1.cpp"
+#elif CUAL_COMPILO==2
+  #include "matriz_bit2.cpp"
+#elif CUAL_COMPILO==3
+  #include "matriz_bit3.cpp"
+#else
+  #include "matriz_bit4.cpp"
+#endif
+
 
 bool Inicializar (MatrizBit& m, int fils, int fils, int cols){
   for (int i=0; i<10; i++){
