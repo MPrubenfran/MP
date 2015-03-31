3b86b33b1a1213e8e8bfec87736ce218d9475219
Rubén Morales Pérez <srmorales@correo.ugr.es>
2015-03-31 12:19:34 +0200

    Create matriz_bit4.h

---
+++ tdas/matriz_bit4.h
@@ -0,0 +1,24 @@
+#ifndef matrizbit3_h
+#define matrizbit3_h
+#include <iostream>
+#include "MatrizBit.h"
+using namespace std;
+struct MatrizBit3{
+  unsigned int matriz[4]n;
+  unsigned short int dimension;
+};
+
+bool Inicializar (MatrizBit& m, int fils, int fils, int cols);
+int Filas (const MatrizBit& m);
+int Columnas (const MatrizBit& m);
+bool Get(const MatrizBit& m,int f,int c);
+void Set(MatrizBit& m,int f,int c,bool v);
+bool Leer(std::istream& is, MatrizBit& m);
+bool Escribir(std::ostream& os,const MatrizBit& m);
+bool Leer(const char nombre[],MatrizBit& m);
+bool Escribir(const char nombre[], const MatrizBit& m);
+void Traspuesta(MatrizBit& res,const MatrizBit& m);
+void And(MatrizBit& res,const MatrizBit& m1,const MatrizBit& m2);
+void Or(MatrizBit& res,const MatrizBit& m1,const MatrizBit& m2);
+void Not(MatrizBit& res,const MatrizBit& m);
+#endif
