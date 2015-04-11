6536cfac761fe14090c0de231268534212f999d8
Rubén Morales Pérez <srmorales@correo.ugr.es>
2015-04-11 13:28:27 +0200

    Update matriz_bit1.h

--- tdas/matriz_bit1.h
+++ tdas/matriz_bit1.h
@@ -10,16 +10,4 @@ struct MatrizBit{
 	bool matriz[10][10];
 };
 
-
-bool Leer(std::istream& is, MatrizBit& m);
-bool Escribir(std::ostream& os,const MatrizBit& m);
-bool Leer(const char nombre[],MatrizBit& m);
-bool Escribir(const char nombre[], const MatrizBit& m);
-void Traspuesta(MatrizBit& res,const MatrizBit& m);
-void And(MatrizBit& res,const MatrizBit& m1,const MatrizBit& m2);
-void Or(MatrizBit& res,const MatrizBit& m1,const MatrizBit& m2);
-void Not(MatrizBit& res,const MatrizBit& m);
-
-
-
 #endif
