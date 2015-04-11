ee77d2cee8da25559f734616b97a6fa8d6f0b96c
fjmpiq <fjmoralespiqueras@gmail.com>
2015-04-11 18:38:35 +0200

    Update matriz_bit4.cpp

--- tdas/matriz_bit4.cpp
+++ tdas/matriz_bit4.cpp
@@ -8,71 +8,54 @@ using namespace std
 
 bool Inicializar (MatrizBit& m, int fils, int cols){
   bool inicializar = true;
-  unsigned short int dim=0, aux=0;
-  if( (fils * cols > 100 )
+  if (fils * cols > 128)
     inicializar = false;
   if (inicializar){
-    for (int i=0; i<fils i++){
-      for (int j=0; j<cols; j++){
-        Set(m,fils, cols, 0);
-      }
+    for (int i=0; i<4; i++){
+      matriz[i]=0;
     }
+    m.dimension = (fils << 8) + cols;
   }
-  aux = fils;
-  aux = (aux << 8); //Desplazo filas y hago el | (OR) con las columnas al final.
-  dim = cols;
-  dim = ( dim | aux );
-
-  m.dimension = dim;
   return inicializar;
 }
+
 int Filas (const MatrizBit& m){
-  int fil = (m.dimension >> 16);
-  return fil;
+  int fils = (m.dimension >> 8);
+  return fils;
 }
+
 int Columnas (const MatrizBit& m){
-  int col = ((m.dimension << 16) >> 16);
-  return col;
+  int cols = (m.dimension << 8) >> 8;
+  return cols;
 }
-bool Get(const MatrizBit& m,int f,int c){
+
+bool Get(const MatrizBit& m, int f, int c){
   char caracter;
   bool casilla;
-  if ( (f< Filas(m)) && (c<Columnas(m) )
-    caracter= m.matriz[f*Columnas(m) + c];
-  else
-    return 0;
-  if (caracter == 1)
-    casilla = true;
-  else
-    casilla = false;
+  int pos, e, d;
+  if ((f < Filas(m)) && (c < Columnas(m)){
+    pos = f*Columnas(m) + c;
+    e = pos / 32;
+    d = pos % 32;
+    casilla = (m.matriz[e] >> d) & 1;
+  }
   return casilla;
 }
 
-void Set(MatrizBit& m, int f,int c,bool v){ ////// CUIDADO
-  unsigned int aux = 1;
-  int numero = f*Columnas(m) +c, numero_aux;
-  int vector=0;
+void Set(MatrizBit& m, int f,int c,bool v){
+  unsigned int aux;
+  int pos, e, d;
+  
   if ( (f < Filas(m)) && (c < Columnas(m) ){
-    while(numero >= 32){ //Nos dirá la posición en la que insertar el booleano dentro de unsigned int matriz[4]
-      numero -= 32;
-      vector++;
-    } //matriz[vector] en el bit "numero"
+    pos = f*Columnas(m) + c;
+    e = pos / 32;
+    d = pos % 32;
+    aux = 1 << d;
     
-    numero_aux = 31-numero;
-    aux = ~ (aux << numero_aux); // 111...101....111;
-    m.matriz[vector] = (m.matriz[vector] & aux);
-    // a b c ... [v] ... y z
-    // 1 1 1 ...  0  ... 1 1 &
-    // a b c ...  0  ... y z -->
-    if (v)
-      aux = 1;
-    else
-      aux = 0;
-    aux = (aux << numero_aux); // 000.1.000 o 000.0.000
-    m.matriz[vector] = (m.matriz[vector] | aux);
-    // a b c ... 0 ... y z
-    // 0 0 0 ... v ... 0 0 |
-    // a b c ... v ... y z
+    matriz[e] = matriz[e] | aux;
+    aux *= v;
+    
+    matriz[e] = matriz[e] & aux;
   }
 }
 
