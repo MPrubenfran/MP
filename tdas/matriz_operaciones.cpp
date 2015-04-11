a4ced40506c10923ade0891394b44c078aba0d5c
fjmpiq <fjmoralespiqueras@gmail.com>
2015-04-11 14:37:58 +0200

    Update matriz_operaciones.cpp

--- tdas/matriz_operaciones.cpp
+++ tdas/matriz_operaciones.cpp
@@ -57,7 +57,7 @@ void Not(MatrizBit& res,const MatrizBit& m){
   filas = Filas(m);
   columnas = Columnas(m);
   
-  Set(res, filas, columnas, aux);
+  Inicializar(res, filas, columnas);
   
   for (int i=0; i < filas; i++){
     for (int j=0; j < columnas; j++){
