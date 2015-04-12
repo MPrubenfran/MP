7f0fead4c2936d9697e3534061b74dfbb095b626
Rubén Morales Pérez <srmorales@correo.ugr.es>
2015-04-12 17:01:50 +0200

    Update matriz_bit3.h

--- tdas/matriz_bit3.h
+++ tdas/matriz_bit3.h
@@ -3,22 +3,10 @@
 #include <iostream>
 #include "MatrizBit.h"
 using namespace std;
-struct MatrizBit3{
+
+struct MatrizBit{
   unsigned int dimension;
   char matriz[100];
 };
 
-bool Inicializar (MatrizBit& m, int fils, int fils, int cols);
-int Filas (const MatrizBit& m);
-int Columnas (const MatrizBit& m);
-bool Get(const MatrizBit& m,int f,int c);
-void Set(MatrizBit& m,int f,int c,bool v);
-bool Leer(std::istream& is, MatrizBit& m);
-bool Escribir(std::ostream& os,const MatrizBit& m);
-bool Leer(const char nombre[],MatrizBit& m);
-bool Escribir(const char nombre[], const MatrizBit& m);
-void Traspuesta(MatrizBit& res,const MatrizBit& m);
-void And(MatrizBit& res,const MatrizBit& m1,const MatrizBit& m2);
-void Or(MatrizBit& res,const MatrizBit& m1,const MatrizBit& m2);
-void Not(MatrizBit& res,const MatrizBit& m);
 #endif
