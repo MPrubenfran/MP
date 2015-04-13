28291d78448e00d9002b96083b0031e91d64d625
fjmpiq <fjmoralespiqueras@gmail.com>
2015-04-13 13:55:48 +0200

    Update matriz_bit1.cpp

--- tdas/matriz_bit1.cpp
+++ tdas/matriz_bit1.cpp
@@ -30,85 +30,13 @@ int Filas (const MatrizBit& m){
 int Columnas (const MatrizBit& m){
   return m.columnas;
 }
-bool Get(const MatrizBit& m,int f,int c){
+bool Get(const MatrizBit& m, int f, int c){
   if ( (f<m.filas) && (c<m.columnas) )
     return m.matriz[f][c];
   else
     return 0;
 }
-void Set(MatrizBit& m, int f,int c,bool v){
+void Set(MatrizBit& m, int f, int c, bool v){
   if ( (f < m.filas) && (c < m.columnas) )
       m.matriz[f][c] = v;
-}
-
-
-/////////////////////////////////////Resto de funcinoes
-bool Leer(std::istream& is, MatrizBit& m){
-  bool exito = true;
-  if (is.fail())
-      exito = false;
-  else{
-      if ( (is.peek() != 'X') && (is.peek() != '.')){ // 1
-        is >> m.filas;
-         is >> m.columnas;
-          if (! is.fail() && m.filas < 11 && m.columnas < 11){
-                for (int i=0; i< m.filas; i++){
-                  for (int j=0; j< m.columnas; j++){
-                    is >> m.matriz[i][j];
-                  }
-                }
-          }
-          else{ 
-                cout << "No se ha podido leer la matriz." << endl;
-                exito = false; 
-          }
-      }
-      else if (is.peek() == 'X' || is.peek() == '.'){ // 2
-        int i=0, j=0, k=0, columnas;
-        char caracter;
-        bool buffer[128];
-        while (is.peek() == 'X' || is.peek() == '.'){
-          j=0; 
-            while(is.peek() != '\n'){
-              is >> caracter;
-              if (caracter == 'X')
-                buffer[k] = true;
-              else
-                buffer[k] = false;
-              
-              j++;
-              k++;
-            }
-            
-          if (i == 0){
-            columnas = j;
-          }
-          else{
-            if (columnas != j)
-               exito = false;
-          }
-          i++;
-          while ( (is.peek() == '\n') || (is.peek() == '\t') || (is.peek() == ' ') )
-            is.get(caracter); // Controlamos la entrada de datos saltando línea
-        }
-        Inicializar(m, i, columnas);
-        for (int l=0; l< Filas(m); l++ ){
-          for (int p=0; p< Columnas(m); p++){
-            Set(m, l, p, buffer[l*Columnas(m) + p]);
-          }
-        }
-      }
-      else  // 3
-       exito = false;
-     
-      
-  }
-  return exito;
-}
-
-bool Leer(const char nombre[],MatrizBit& m){
-  ifstream is(nombre);
-  bool exito = Leer(is, m);
-  
-  return exito;
 }
